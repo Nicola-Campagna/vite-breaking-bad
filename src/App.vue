27796131c3a9a69cf01dcd4ea8d81a7fbc331439
Nicola-Campagna <campagnanicola379@gmail.com>
2023-03-02 11:25:24 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            title: "hello"
+        }
+    }
+}
+
+</script>
+<template>
+    <h1 class="text-center">{{ title }}</h1>
+</template>
+<style lang="scss"></style>
+
+
+
