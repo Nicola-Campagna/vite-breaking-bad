ede2df335fb41f544d89c767e5ad3aba86b3e17d
Nicola-Campagna <campagnanicola379@gmail.com>
2023-03-03 11:47:22 +0100

    add header + folder and file scss

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script>
+// importiamo i file di altri componenti
+import AppHeader from "./components/AppHeader.vue"
 
 export default {
   data() {
@@ -7,10 +9,34 @@ export default {
   },
 
   components: {
+    // dichiarare i componenti che importiamo
+    AppHeader,
+
   },
 };
 </script>
 
-<template></template>
+<template>
+  <!-- stampiamo i componenti importati -->
 
-<style lang="scss"></style>
+  <!-- HEADER -->
+
+  <header>
+    <div class="container-fluid bg-light border py-4">
+      <AppHeader />
+
+    </div>
+  </header>
+
+  <!-- MAIN -->
+  <main>
+    <div class="container-fluid bg-orange">
+    </div>
+
+  </main>
+</template>
+
+<style lang="scss">
+// use per usare le rgole del file style.scss con le variabili
+@use "./scss/partials/style.scss" as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+    data() {
+        return {
+            title: "YU-GI-OH Api"
+        }
+    }
+
+    // components: {
+    //   MyComponent,
+    // },
+};
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+    <!-- da inserire logo (icona succesivam.) -->
+</template>
+
+<style lang="scss" scoped></style>
